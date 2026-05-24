5e852e6a82e73c3271c7ba6c58eb9d009adb915a
yiming <yiming9137@163.com>
2026-05-24 11:19:04 +0800

    索引

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Yiming9137's Blog";
+export const SITE_DESCRIPTION = 'Yiming9137 的个人博客，分享技术与生活';
